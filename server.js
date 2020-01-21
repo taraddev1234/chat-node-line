3c45d625ae1c65a721be4027ffb8c4d7d53a1b80
SaifaN <sayfa@tarad.com>
2020-01-21 16:17:35 +0700

    $$$inititialize

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+//var serv = require("socket.io");
+const app = express()
+var PORT = process.env.PORT || 80;
+//var io = serv.listen(80);  // เปิดเซิฟเวอร์ที่ port 9999
+var server = app.listen(PORT, () =>{
+    console.log('Start server at port '+PORT)
+})
+
+app.get('/',(req,res) =>{
+    res.send('Hello World2')
+})
+
+app.get('/saifa',(req,res) => {
+    res.send('Hello Saifa')
+})
